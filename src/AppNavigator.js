1d06761062e08f6ad8218f9cc6764d4ae6fa4105
kedar09 <32976533+kedar09@users.noreply.github.com>
2020-10-28 23:45:50 +0530

    Update AppNavigator.js

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomePage from './components/Pages/Home/HomePage';
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomePage}
-          options={{ title: 'Crud Operation' }}
-        />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomePage}
+                    options={{title: 'Crud Operation'}}
+                />
+                {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
